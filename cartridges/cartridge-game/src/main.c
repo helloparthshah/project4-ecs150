bbde26246711f2e370750c2c0b1f184e90df70bd
Parth Shah <helloparthshah@gmail.com>
2021-11-28 14:26:39 -0800

    changes

--- cartridges/cartridge-game/src/main.c
+++ cartridges/cartridge-game/src/main.c
@@ -72,6 +72,7 @@ int main() {
     WriteString("Done\nStarting");
     RenderHero(&HeroState,HeroFrames,MainPalette);
     RVCSetVideoUpcall(VideoUpcall,NULL);
+    RVCChangeVideoMode(RVCOS_VIDEO_MODE_GRAPHICS);
     
     while(1){
         FrameDelay();
