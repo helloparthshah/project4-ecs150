ba1eeaa94cc6edea97366b9d8b6ff06b60b574da
Parth Shah <helloparthshah@gmail.com>
2021-11-28 14:03:40 -0800

    changes

--- rvcos/src/graphics.c
+++ rvcos/src/graphics.c
@@ -108,11 +108,16 @@ TStatus RVCChangeVideoMode(TVideoMode mode) {
 
 extern volatile TUpcallPointer UpcallPointer;
 extern volatile void *UpcallParam;
+int upcall_flag = 0;
 
 TStatus RVCSetVideoUpcall(TThreadEntry upcall, void *param) { 
-  UpcallPointer = (TUpcallPointer)upcall;
-  UpcallParam = (void *)param;
-  return RVCOS_STATUS_SUCCESS;
+  if (upcall_flag == 0) {
+    UpcallPointer = (TUpcallPointer)upcall;
+    UpcallParam = (void*)param;
+    upcall_flag = 1;
+    return RVCOS_STATUS_SUCCESS;
+  }
+  return RVCOS_STATUS_FAILURE;
 }
 
 int nBg = 0;
@@ -220,7 +225,7 @@ TStatus RVCGraphicDraw(TGraphicID gid, SGraphicPositionRef pos,
   if (gid < 4) {
     for(int i=0;i<288;i++){
       // if(src+srcwidth*i+dim->DWidth!=NULL)
-      memcpy((void*)BackgroundDataBuffer[gid] + i*512, src, 512);
+      memcpy((void*)BackgroundDataBuffer[gid] + i*512, src+i*srcwidth, 512);
     }
     // memcpy((void *)BackgroundDataBuffer[gid], src, srcwidth);
   } else if (gid < 68) {
